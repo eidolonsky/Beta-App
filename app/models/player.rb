8f7b78987f13f79e1965873f4ef51ea819c91d5d
Max King <maxking@dhcp168.merusouth127.iit.edu>
2016-10-03 17:07:36 -0700

    Syntax change on associations in models to fix error messages.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,4 @@
 class Player < ApplicationRecord
   has_many :rosters
-  has_many :teams, through :rosters
+  has_many :teams, :through => :rosters
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,4 @@
 class Team < ApplicationRecord
   has_many :rosters
-  has_many :players, through :rosters
+  has_many :players, :through => :rosters
 end
