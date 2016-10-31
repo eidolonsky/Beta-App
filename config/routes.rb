8ae1610beaa26ef1f6a7be5b55b85e6d225c57cf
Max King <maxking@dhcp105.merusouth118.iit.edu>
2016-10-31 15:58:38 -0700

    Added a few specificiations to omniauth and fixed typo on URL path

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :teams
   resources :players
   post 'players/pick'
-  get 'auth/:provider', to: 'sessions#create', as: 'login_path'
+  get 'auth/:provider/callback', to: 'sessions#create', as: 'login_path'
   get '/auth/google_oauth2', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
