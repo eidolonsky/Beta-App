b44a596a055da8c4e74daafbb6c54e923e47faec
Max King <maxking@dhcp105.merusouth118.iit.edu>
2016-10-31 16:47:00 -0700

    Created basic controller function to output data.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,13 @@
 class SessionsController < ApplicationController
-  #def create
-  #  @user = User.from_omniauth(request.env['omniauth.auth'])
-  #  session[:id] = User.id
-  #end
-  def create 
-    render text: request.env['omniauth.auth'].to_yaml
+  def create
+    begin 
+      @user = User.from_omniauth(request.env['omniauth.auth'])
+      session[:id] = User.id
+      flash[:success] = "Welcome, #{@user.name}!"
+    rescue
+      flash[:warning] = "There was an error while trying to authenticate you..."
+    end
+    redirect_to root
   end
   def destroy
   end
