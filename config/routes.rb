18daf9a10821fd8efac95d24631733ba8e8a4ba2
Max King <maxking@dhcp68.merusouth125.iit.edu>
2016-11-01 14:41:59 -0700

    Added destroy login session route. Still not working.

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   post 'players/pick'
   get 'auth/:provider/callback', to: 'sessions#create'
   get '/auth/google_oauth2', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
