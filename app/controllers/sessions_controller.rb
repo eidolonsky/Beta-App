07b2b599a7d4bfac19f36d4a4b0c669657985877
Max King <maxking@dhcp68.merusouth125.iit.edu>
2016-11-01 14:34:27 -0700

    Slimmed down controller logic to be in line with Karls

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,8 @@
 class SessionsController < ApplicationController
   def create
-    begin 
-      @user = User.from_omniauth(request.env['omniauth.auth'])
-      session[:id] = User.id
-      flash[:success] = "Welcome, #{@user.name}!"
-    rescue
-      flash[:warning] = "There was an error while trying to authenticate you..."
-    end
-    redirect_to root
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    session[:id] = User.ids
+    redirect_to root_path
   end
   def destroy
   end
