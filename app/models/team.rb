85b7bedaa7b24599ae24317da52d8c612ba164a6
Max King <maxking@dhcp166.merusouth115.iit.edu>
2016-11-14 15:05:12 -0800

    changes to schema and team model to incorporate user reference

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,7 @@
 class Team < ApplicationRecord
   validates :name, presence: true
   validates :manager, presence: true
+  validates :user_id, presence: true
   has_many :rosters
   has_many :players, :through => :rosters
   belongs_to :user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161101021224) do
+ActiveRecord::Schema.define(version: 20161114225807) do
 
   create_table "players", force: :cascade do |t|
     t.string   "name"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 20161101021224) do
     t.string   "manager"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_teams_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
