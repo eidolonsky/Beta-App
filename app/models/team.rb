7adaed63d84228d1bcdeb541d3e75bc9edb95428
Max King <maxking@dhcp166.merusouth115.iit.edu>
2016-11-14 16:00:33 -0800

    Forcing validation against team reference to user, as a team must have a user

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,6 @@
 class Team < ApplicationRecord
   validates :name, presence: true
   validates :manager, presence: true
-  validates :user_id, presence: true
   has_many :rosters
   has_many :players, :through => :rosters
   belongs_to :user
